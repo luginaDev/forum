016e59a8ab232497768a8c5ab23ec4669ca97769
luginaDev <luginaDev@gmail.com>
2020-07-23 23:12:53 +0700

    adding author info and fixed n+1 query problem

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -4,15 +4,22 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card">
+            <div class="card grid-margin grid-margin-xl-0 stretch-card">
                 <div class="card-header">{{ __('All Questions') }}</div>
 
                 <div class="card-body">
                    @foreach ($questions as $question)
                        <div class="media">
                             <div class="media-body">
-                                <h3 class="mt-0">{{ $question->title }}</h3>
+                                <h4 class="mt-0"><a href="{{ $question->url }}" class="text-primary">{{ $question->title }}</a></h4>
+                                <p class="lead">
+                                    Asked By
+                                    <a href="{{ $question->user->url }}" class="text-primary">{{ $question->user->name }}</a>
+                                    <small class="text-info">{{ $question->created_date }}</small>
+                                </p>
+
                                 {{ \Illuminate\Support\Str::limit($question->body, 250 )}}
+                               
                             </div>
                        </div>
                        <hr>
